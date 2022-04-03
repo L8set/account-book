3442d33275ebec4b406af23906526ec719c180ab
Latte723 <nachikun.13@gmail.com>
2022-04-03 22:13:42 +0900

    購入履歴モデルの修正

--- app/accountbook/models.py
+++ app/accountbook/models.py
@@ -1,45 +1,56 @@
 from pyexpat import model
+from statistics import mode
 from sys import modules
 from django.db import models
 
+# 作成・更新・削除日付の抽象クラス
+class TimeStampedModel(models.Model):
+    create_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(null=True, auto_now=True)
+    delete_date = models.DateField(null=True, blank=True)
+    
+    class Meta:
+        abstract = True
+
 # 用途テーブル
-class Usecase(models.Model):
+class Usecase(TimeStampedModel):
     name = models.CharField(max_length=20, unique=True)
 
 # 人物テーブル
-class Person(models.Model):
+class Person(TimeStampedModel):
     name = models.CharField(max_length=20, unique=True)
 
 # 娯楽テーブル
-class Pastime(models.Model):
+class Pastime(TimeStampedModel):
     name = models.CharField(max_length=20, unique=True)
     start_date = models.DateField()
     end_date = models.DateField()
 
 # 支払い方法テーブル
-class PaymentMethod(models.Model):
+class PaymentMethod(TimeStampedModel):
     name = models.CharField(max_length=20, unique=True)
 
 # 銀行口座テーブル
-class BankAccount(models.Model):
+class BankAccount(TimeStampedModel):
     name = models.CharField(max_length=20, unique=True)
+
 # 決済方法テーブル
-class BillingMethod(models.Model):
+class BillingMethod(TimeStampedModel):
     name = models.CharField(max_length=20, unique=True)
-    payment_method_id = models.ForeignKey(PaymentMethod)
-    bank_account_from = models.ForeignKey(BankAccount)
-    bank_account_to = models.ForeignKey(BankAccount)
+    payment_method = models.ForeignKey(PaymentMethod, on_delete=models.PROTECT)
+    bank_account_from = models.ForeignKey(BankAccount, on_delete=models.PROTECT, related_name='payer')
+    bank_account_to = models.ForeignKey(BankAccount, on_delete=models.PROTECT, related_name='payee')
 
 # 購入履歴テーブル
-class PurchaseHistory(models.Model):
+class PurchaseHistory(TimeStampedModel):
     purchase_date = models.DateField()
-    usecase_id = models.ForeignKey(Usecase)
+    usecase = models.ForeignKey(Usecase, on_delete=models.PROTECT)
     store_name = models.CharField(max_length=30)
     item_name = models.CharField(max_length=30)
-    billing_method_id = models.ForeignKey(BillingMethod)
+    billing_method = models.ForeignKey(BillingMethod, on_delete=models.PROTECT)
     price = models.IntegerField()
-    person_id = models.ForeignKey(Person, null=True)
+    person = models.ForeignKey(Person, null=True, on_delete=models.PROTECT)
     payment_month = models.DateField(null=True)
     note = models.CharField(max_length=30, null=True)
-    pastime_id = models.ForeignKey(Pastime)
+    pastime = models.ForeignKey(Pastime, null=True, on_delete=models.PROTECT)
 
--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accountbook.apps.AccountbookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
